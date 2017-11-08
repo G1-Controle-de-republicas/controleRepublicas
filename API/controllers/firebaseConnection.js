8555ed3308fb48d8f9724731dac4f7f4e70c5455
LeooSimoes <leonardo_leo_9@hotmail.com>
2017-11-08 14:37:41 -0200

    Css perfil alterado

--- API/controllers/firebaseConnection.js
+++ API/controllers/firebaseConnection.js
@@ -1,9 +1,5 @@
 var admin = require("firebase-admin");
-<<<<<<< HEAD
 var serviceAccount = require("../../justdoit-2017-firebase-adminsdk-5x949-7c9448a5c5.json");
-=======
-var serviceAccount = require("../../justdoit-2017-firebase-adminsdk-5x949-b2e298584f");
->>>>>>> 26a45e61b83399048ff92015339d8e3fcddc1a3e
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
