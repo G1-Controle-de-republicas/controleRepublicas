70086198d8cb78ba1888d4a383553d82848f8c65
Bruno <brunocandelabro@gmail.com>
2017-11-10 13:09:58 -0200

    CSS PERFIL ARRUMADO

--- API/controllers/firebaseConnection.js
+++ API/controllers/firebaseConnection.js
@@ -1,5 +1,5 @@
 var admin = require("firebase-admin");
-var serviceAccount = require("../../justdoit-2017-firebase-adminsdk-5x949-7c9448a5c5.json");
+var serviceAccount = require("../../justdoit-2017-firebase-adminsdk-5x949-b2e298584f.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
