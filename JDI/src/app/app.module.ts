9567f05e2c7f6723539ba93192425230cbcb0cc6
LeooSimoes <leonardo_leo_9@hotmail.com>
2017-11-15 15:01:14 -0200

    ué

--- JDI/src/app/app.module.ts
+++ JDI/src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { TextMaskModule } from 'angular2-text-mask';
-
+import { TextMaskModule } from 'angular2- text-mask';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app-routing.module';
