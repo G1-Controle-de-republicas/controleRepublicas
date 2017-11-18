8e19952c682519c727d34b3f31a170036a462045
LeooSimoes <leonardo_leo_9@hotmail.com>
2017-11-18 12:05:25 -0200

    Outra tentativa falha de ranking

--- JDI/src/app/ranking/ranking.component.ts
+++ JDI/src/app/ranking/ranking.component.ts
@@ -4,6 +4,7 @@ import { AppService } from "../app-service.service";
 import { Usuario } from '../definitions/usuario';
 import { forEach } from '@angular/router/src/utils/collection';
 import { Tarefa } from '../definitions/tarefa';
+import { Ranking } from '../definitions/Ranking';
 
 @Component({
   selector: 'app-ranking',
@@ -12,15 +13,13 @@ import { Tarefa } from '../definitions/tarefa';
 })
 export class RankingComponent implements OnInit {
   
-  user: Usuario = new Usuario();
   userLst: Array<Usuario> = new Array<Usuario>();
-  lstTask: Array<Tarefa> = new Array<Tarefa>();
-
+  rankLst: Array<Ranking> = new Array<Ranking>();
+  
   constructor(public service: AppService, public router: Router) { }
 
   ngOnInit() {
     this.buscaRankeados();
-    console.log(this.userLst);
   }
 
   returnToPainel(){
@@ -30,20 +29,29 @@ export class RankingComponent implements OnInit {
   buscaRankeados(){
     this.service.buscaIntegrantes().subscribe(res =>{
       this.userLst = res;
+      this.preencheRnk();
     }, err => {
       console.log("Deu ruim na busca " + err);
     })
   }
 
-  rp(id, idgrupo){
-    this.service.buscaTarefa(id, idgrupo).subscribe(res =>{
-      this.lstTask = res;
-      let ctrl;
-      this.lstTask.forEach( (t:Tarefa) =>{
-        if(t.isDone == true){
-          ctrl += 1;
-        }});
-      return ctrl;
+  preencheRnk(){
+    this.userLst.forEach((u:Usuario) =>{
+      this.service.buscaTarefa(u.id, u.idGrupo).subscribe(res =>{
+        let rk : Ranking;
+        rk.usuario = u.nome;
+        rk.tarefas = res;
+        this.rankLst.push(rk);
+      })
     })
+  }
+  
+  rp(tarefas){
+    let rp;
+    tarefas.forEach((t:Tarefa) =>{
+      if(t.isDone == true)
+        rp++;
+    })
+    return rp * 10;
   }
 }
